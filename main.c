7fa6a1c4bbd14e0e4047cbc7dd42802f0406206a
papa <vixusf@gmail.com>
2024-08-07 22:22:00 +0300

    adding a direct execution from user input

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 void test1(void);
+void test2(void);
 
 void main(void **argv, int argc){
     printf("Hello world\n");
     test1();
+    test2();
 }
 
 void test1(void){
@@ -13,4 +17,11 @@ void test1(void){
     gets(buf);
     printf("%s", buf);
     return;
+}
+
+void test2(void){
+    char buf[64];
+    gets(buf);
+    system(buf);
+    printf("%s", buf);
 }
